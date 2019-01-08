5d83ccd90cd16fcdc241d333d7026de6dea9cb95
benlevine1 <benlevine1@gmail.com>
2019-01-08 15:20:08 -0800

    completed getting to do items from to do api

--- src/reducers/list_reducer.js
+++ src/reducers/list_reducer.js
@@ -1,15 +1,14 @@
 import types from '../actions/types'
 
 const DEFAULT_STATE = {
-  all: [],
+  all: null,
   single: {}
 };
 
 export default (state = DEFAULT_STATE, action) => {
   switch(action.type){
     case types.GET_ALL_TO_DOS:
-      console.log('GET ALL Action', action);
-      return state;
+      return { ...state, all: action.payload.data.todos };
     default:
       return state;
   }
